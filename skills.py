4abbd2678c58d859a7bbe504647c49c1293efc72
andrew <andrew.timko@cofix.global>
2022-11-21 20:18:44 +0600

    First upload

---
+++ skills.py
@@ -0,0 +1,13 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', 'ru')
+engine.setProperty('rate', 180)
+for voice in voices:
+    if voice.name == "Irina":
+        engine.setProperty("voice", voice.id)
+
+def speaker(text):
+    engine.say(text)
+    engine.runAndWait()
