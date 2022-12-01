a41f039e996618a24493665a95d08966964ad556
andrew <andrew.timko@cofix.global>
2022-12-01 16:48:46 +0600

    update words

--- skills.py
+++ skills.py
@@ -1,18 +1,18 @@
-# import pyttsx3
+import pyttsx3
 
-# engine = pyttsx3.init()
-# voices = engine.getProperty('voices')
-# engine.setProperty('voice', 'ru')
-# engine.setProperty('rate', 180)
-# for voice in voices:
-#     if voice.name == "Irina":
-#         engine.setProperty("voice", voice.id)
-#
-# def speaker(text):
-#     engine.say(text)
-#     engine.runAndWait()
-
-import os
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', 'ru')
+engine.setProperty('rate', 180)
+for voice in voices:
+    if voice.name == "Irina":
+        engine.setProperty("voice", voice.id)
 
 def speaker(text):
-    os.system(f"echo «{text}» | RHVoice-test -p anna")
+    engine.say(text)
+    engine.runAndWait()
+
+# import os
+#
+# def speaker(text):
+#     os.system(f"echo «{text}» | RHVoice-test -p anna")
